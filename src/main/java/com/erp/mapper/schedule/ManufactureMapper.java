6e44efdd5b0d3f78b4d70f29a34790901dbd017c
xufeng <aert1094@qq.com>
2019-05-17 15:15:51 +0800

    xf 的逆向工程生成bean

---
+++ src/main/java/com/erp/mapper/schedule/ManufactureMapper.java
@@ -0,0 +1,30 @@
+package com.erp.mapper.schedule;
+
+import com.erp.bean.schedule.Manufacture;
+import com.erp.bean.schedule.ManufactureExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ManufactureMapper {
+    long countByExample(ManufactureExample example);
+
+    int deleteByExample(ManufactureExample example);
+
+    int deleteByPrimaryKey(String manufactureSn);
+
+    int insert(Manufacture record);
+
+    int insertSelective(Manufacture record);
+
+    List<Manufacture> selectByExample(ManufactureExample example);
+
+    Manufacture selectByPrimaryKey(String manufactureSn);
+
+    int updateByExampleSelective(@Param("record") Manufacture record, @Param("example") ManufactureExample example);
+
+    int updateByExample(@Param("record") Manufacture record, @Param("example") ManufactureExample example);
+
+    int updateByPrimaryKeySelective(Manufacture record);
+
+    int updateByPrimaryKey(Manufacture record);
+}
