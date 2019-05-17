8e0a769c64e4631dc5c9023712798a5b42640cc4
alan-ZCH <1136559229@qq.com>
2019-05-17 23:07:09 +0800

    提交部分内容

--- src/main/java/com/erp/bean/quality/PageHelper.java
+++ src/main/java/com/erp/bean/quality/PageHelper.java
@@ -1,4 +1,4 @@
-package com.erp.bean.quality;
+package model.admin;
 
 import java.util.List;
 
@@ -9,10 +9,77 @@ public class PageHelper<T> {
     private int totalPageNum; //总页数
     private int prevPageNum; //前一页
     private int nextPageNum; //后一页
-    private List<T> records;
+    private List<T> tList; //这里为什么要使用泛型？使用泛型意味着PageHelper可以在不同的场合下使用，而不是只能在分类的情况下使用
 
     public PageHelper() {
     }
 
+    public PageHelper(int totalRecordsNum, int currentPageNum, int totalPageNum, int prevPageNum, int nextPageNum, List<T> tList) {
+        this.totalRecordsNum = totalRecordsNum;
+        this.currentPageNum = currentPageNum;
+        this.totalPageNum = totalPageNum;
+        this.prevPageNum = prevPageNum;
+        this.nextPageNum = nextPageNum;
+        this.tList = tList;
+    }
 
+    public int getTotalRecordsNum() {
+        return totalRecordsNum;
+    }
+
+    public void setTotalRecordsNum(int totalRecordsNum) {
+        this.totalRecordsNum = totalRecordsNum;
+    }
+
+    public int getCurrentPageNum() {
+        return currentPageNum;
+    }
+
+    public void setCurrentPageNum(int currentPageNum) {
+        this.currentPageNum = currentPageNum;
+    }
+
+    public int getTotalPageNum() {
+        return totalPageNum;
+    }
+
+    public void setTotalPageNum(int totalPageNum) {
+        this.totalPageNum = totalPageNum;
+    }
+
+    public int getPrevPageNum() {
+        return prevPageNum;
+    }
+
+    public void setPrevPageNum(int prevPageNum) {
+        this.prevPageNum = prevPageNum;
+    }
+
+    public int getNextPageNum() {
+        return nextPageNum;
+    }
+
+    public void setNextPageNum(int nextPageNum) {
+        this.nextPageNum = nextPageNum;
+    }
+
+    public List<T> gettList() {
+        return tList;
+    }
+
+    public void settList(List<T> tList) {
+        this.tList = tList;
+    }
+
+    @Override
+    public String toString() {
+        return "PageHelper{" +
+                "totalRecordsNum=" + totalRecordsNum +
+                ", currentPageNum=" + currentPageNum +
+                ", totalPageNum=" + totalPageNum +
+                ", prevPageNum=" + prevPageNum +
+                ", nextPageNum=" + nextPageNum +
+                ", tList=" + tList +
+                '}';
+    }
 }
