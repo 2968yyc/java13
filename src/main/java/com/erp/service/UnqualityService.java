b174e9b1a15753144d8f34da8a297a024be3c654
alan-ZCH <1136559229@qq.com>
2019-05-17 22:15:15 +0800

    修改bean、接口和映射文件

---
+++ src/main/java/com/erp/service/UnqualityService.java
@@ -0,0 +1,9 @@
+package com.erp.service;
+
+
+import com.erp.bean.quality.Unqualify;
+import java.util.List;
+
+public interface UnqualityService {
+    List<Unqualify> findAllUnqualify();
+}
---
+++ src/main/java/com/erp/service/impl/UnqualityServiceImpl.java
@@ -0,0 +1,23 @@
+package com.erp.service.impl;
+
+import com.erp.bean.quality.Unqualify;
+import com.erp.mapper.quality.UnqualifyMapper;
+import com.erp.service.UnqualityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class UnqualityServiceImpl implements UnqualityService {
+
+    @Autowired
+    UnqualifyMapper unqualifyMapper;
+
+    public List<Unqualify> findAllUnqualify(){
+        return unqualifyMapper.selectAllUnqualify();
+    }
+
+
+
+}
