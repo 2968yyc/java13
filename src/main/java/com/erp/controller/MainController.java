5de9a3838e38cd6aaad7a695127c83d86cdb98a3
pikachu <13298328928@163.com>
2019-05-17 14:01:18 +0800

    添加主方法

--- src/main/java/com/erp/controller/MainController.java
+++ src/main/java/com/erp/controller/MainController.java
@@ -1,12 +1,19 @@
 package com.erp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Author: Qiu
  * @Date: 2019/5/17 13:10
  */
 
-
+@Controller
 public class MainController {
 
+    @RequestMapping("home")
+    public String toHome(){
+        return "home";
+    }
 
 }
