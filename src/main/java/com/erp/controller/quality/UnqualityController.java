f31a1073679b19e3072649f22407ea29771ee41a
alan-ZCH <1136559229@qq.com>
2019-05-18 07:31:44 +0800

    解决视图解释器无法解释的问题

--- src/main/java/com/erp/controller/quality/UnqualityController.java
+++ src/main/java/com/erp/controller/quality/UnqualityController.java
@@ -1,24 +1,14 @@
 package com.erp.controller.quality;
 
-import com.erp.service.UnqualityService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("unqualify")
 public class UnqualityController {
 
-    @Autowired
-    UnqualityService unqualityService;
-
     @RequestMapping("find")
-    public String find(Model model){
+    public String find(){
         return "unqualify_list";
     }
-    /*public List<Unqualify> findAllUnquality(){
-        List<Unqualify> unqualifies = unqualityService.selectAllUnqualify();
-        return unqualifies;
-    }*/
 }
