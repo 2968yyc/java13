ea72c2349467f8e573f3b71b56b3cddc1f2fd8f4
xufeng <aert1094@qq.com>
2019-05-20 15:34:23 +0800

    xf  V.1.3.1 文件下载

--- src/main/java/com/erp/controller/MainController.java
+++ src/main/java/com/erp/controller/MainController.java
@@ -1,14 +1,16 @@
 package com.erp.controller;
 
 import com.erp.bean.UploadInfo;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -82,27 +84,72 @@ public class MainController {
         return map;
     }
 
-    @RequestMapping("file/upload")
-    public @ResponseBody
-    UploadInfo uploadfile( @RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request) {
-        String fileName=file.getOriginalFilename();
-        String path = request.getSession().getServletContext().getRealPath("/WEB-INF/file/");
-        String suffix = fileName.substring(fileName.lastIndexOf("."));
-        String prefix = fileName.substring(0, fileName.lastIndexOf("."));
-        long time = new Date().getTime();
-        String newFileName = prefix + time + suffix;
-        File newFile = new File(path + newFileName);
-        if (!newFile.getParentFile().exists()) {
-            newFile.getParentFile().mkdir();
-        }
+
+    /*public void downloadfile(HttpServletRequest request,HttpServletResponse response) throws Exception{  
+        //模拟文件，myfile.txt为需要下载的文件  
+        String fileName = request.getSession().getServletContext().getRealPath("upload")+"/myfile.txt";  
+        //获取输入流  
+        InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));  
+        //假如以中文名下载的话  
+        String filename = "下载文件.txt";  
+        //转码，免得文件名中文乱码  
+        filename = URLEncoder.encode(filename,"UTF-8");  
+        //设置文件下载头  
+        response.addHeader("Content-Disposition", "attachment;filename=" + filename);    
+        //1.设置文件ContentType类型，这样设置，会自动判断下载文件类型    
+        response.setContentType("multipart/form-data");   
+        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());  
+        int len = 0;  
+        while((len = bis.read()) != -1){  
+            out.write(len);  
+            out.flush();  
+        }  
+        out.close();  
+    } */
+
+    @RequestMapping("file/download")
+    public void downloadFile(String fileName,HttpServletRequest req, HttpServletResponse resp){
+        String filepath=req.getSession().getServletContext().getRealPath("/WEB-INF/")+fileName;
+        InputStream bis=null;
+        BufferedOutputStream out=null;
         try {
-            file.transferTo(newFile);
-            return new UploadInfo(0, "file/"+newFileName);
-        } catch (IOException e) {
+            bis=new BufferedInputStream(new FileInputStream(new File(filepath)));
+            String name=fileName.substring(fileName.lastIndexOf("/"));
+            name= URLEncoder.encode(name,"utf-8");
+            resp.addHeader("Content-Disposition","attachment;filename="+name);
+            resp.setContentType("multipart/form-data");
+             out= new BufferedOutputStream(resp.getOutputStream());
+            byte[] b=new byte[2048];
+            int len=0;
+            while((len = bis.read(b))!=-1){
+                out.write(b,0,len);
+                out.flush();
+            }
+
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return new UploadInfo(1, null,"网络异常，请重新上传");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(bis!=null){
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(out!=null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
     }
+
+
 
     @RequestMapping("file/delete")
     @ResponseBody
